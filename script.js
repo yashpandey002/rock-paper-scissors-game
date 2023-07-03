bea1aeea336d8e6e3608f96d709d78185e4d62fc
Yash Pandey <pandeyyash002@gmail.com>
2023-07-03 22:39:09 +0530

    Connected JS logic with UI

--- script.js
+++ script.js
@@ -5,105 +5,143 @@ const getComputerChoice = function () {
     let computerChoice = '';
     switch (randNum) {
         case 1:
-            computerChoice = 'Rock';
+            computerChoice = '✊';
             break;
         case 2:
-            computerChoice = 'Paper';
+            computerChoice = '✋';
             break;
         case 3:
-            computerChoice = 'Scissors';
+            computerChoice = '✌';
             break;
     }
 
     return computerChoice;
 };
 
-const playSingleGame = function () {
-    // All posibilities
-    // rock against paper
-    // paper against rock
-    // paper against scissors
-    // scissors against paper
-    // scissors against rock
-    // rock against scissors
-
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt('Enter your choice: ');
-
-    // Check for Rock and Paper
-    // rock against paper
-    // paper against rock
-    if (
-        playerSelection.toLowerCase() == 'rock' &&
-        computerSelection == 'Paper'
-    ) {
-        console.log('You Lose! Paper beats Rock');
-        return 'computer';
-    } else if (
-        playerSelection.toLowerCase() == 'paper' &&
-        computerSelection == 'Rock'
-    ) {
-        console.log('You Won! Paper beats Rock');
-        return 'player';
+const playSingleGame = function (playerChoice, computerChoice) {
+    // Check for Rock✊ and Paper✋
+    if (playerChoice === '✊' && computerChoice === '✋') {
+        return [
+            {
+                winningMessage: 'You Lose! Paper beats Rock',
+                winner: 'computer',
+            },
+        ];
+    } else if (playerChoice === '✋' && computerChoice === '✊') {
+        return [
+            {
+                winningMessage: 'You Won! Paper beats Rock',
+                winner: 'player',
+            },
+        ];
     }
 
-    // Check for Paper and Scissors
-    // paper against scissors
-    // scissors against paper
-    else if (
-        playerSelection.toLowerCase() == 'paper' &&
-        computerSelection == 'Scissors'
-    ) {
-        console.log('You Lose! Scissors beats Paper');
-        return 'computer';
-    } else if (
-        playerSelection.toLowerCase() == 'scissors' &&
-        computerSelection == 'Paper'
-    ) {
-        console.log('You Won! Scissors beats Paper');
-        return 'player';
+    // Check for Rock✊ and Scissors✌
+    else if (playerChoice === '✊' && computerChoice === '✌') {
+        return [
+            {
+                winningMessage: 'You Won! Rock beats Scissors',
+                winner: 'player',
+            },
+        ];
+    } else if (playerChoice === '✌' && computerChoice === '✊') {
+        return [
+            {
+                winningMessage: 'You Lose! Rock beats Scissors',
+                winner: 'computer',
+            },
+        ];
     }
 
-    // Check for Rock and Scissors
-    // scissors against rock
-    // rock against scissors
-    else if (
-        playerSelection.toLowerCase() == 'rock' &&
-        computerSelection == 'Scissors'
-    ) {
-        console.log('You Won! Rock beats Scissors');
-        return 'player';
-    } else if (
-        playerSelection.toLowerCase() == 'scissors' &&
-        computerSelection == 'Rock'
-    ) {
-        console.log('You Lose! Rock beats Scissors');
-        return 'computer';
+    // Check for Paper✋ and Scissors✌
+    else if (playerChoice === '✋' && computerChoice === '✌') {
+        return [
+            {
+                winningMessage: 'You Lose! Scissors beats Paper',
+                winner: 'computer',
+            },
+        ];
+    } else if (playerChoice === '✌' && computerChoice === '✋') {
+        return [
+            {
+                winningMessage: 'You Won! Scissors beats Paper',
+                winner: 'player',
+            },
+        ];
     }
 
     // If both have same signs
     else {
-        console.log('Game tied');
-        return 'tied';
+        return [
+            {
+                winningMessage: 'Game tied!',
+                winner: 'tied',
+            },
+        ];
     }
 };
 
-const game = function () {
-    let compWinningCount = 0;
-    let playerWinningCount = 0;
-    let gameLength = 5;
-    for (let i = 0; i < gameLength; i++) {
-        const currentWinner = playSingleGame();
+let gameCount = 0;
+let playerWinningCount = 0;
+let computerWinningCount = 0;
+const optionsPlayerHave = document.querySelectorAll('.option__box');
+optionsPlayerHave.forEach((option) => {
+    option.addEventListener('click', () => {
+        const currentGameUpdate = document.querySelector('.update');
+        const playerChoiceEl = document.getElementById('player-choice');
+        const computerChoiceEl = document.getElementById('computer-choice');
+        const playerScore = document.getElementById('player-score');
+        const computerScore = document.getElementById('computer-score');
+        const gameOverBox = document.querySelector('.game-over-box');
+        const finalWinner = document.getElementById('final-winner');
+        const playerChoice = option.getAttribute('id');
+        const computerChoice = getComputerChoice();
 
-        if (currentWinner === 'player') {
-            playerWinningCount++;
-        } else if (currentWinner == 'computer') {
-            compWinningCount++;
-        } else if (currentWinner == 'tied') {
-            gameLength++;
+        playerChoiceEl.textContent = playerChoice;
+        computerChoiceEl.textContent = computerChoice;
+
+        if (gameCount <= 5) {
+            let [currentResults] = playSingleGame(playerChoice, computerChoice);
+            let currentWinner = currentResults.winner;
+            let currentWinningMessage = currentResults.winningMessage;
+
+            // Only go in this statement if the winning message is NOT tied
+            if (currentWinningMessage !== 'Game tied!') {
+                currentGameUpdate.textContent = currentWinningMessage;
+                currentGameUpdate.style.color = '#f8e700';
+                if (currentWinner === 'player') {
+                    playerWinningCount++;
+                    playerScore.textContent = playerWinningCount;
+                } else if (currentWinner === 'computer') {
+                    computerWinningCount++;
+                    computerScore.textContent = computerWinningCount;
+                }
+
+                // Incrementing the game count with 1
+                gameCount++;
+            }
+            // If game is tied go in this statement
+            else {
+                currentGameUpdate.textContent = currentWinningMessage;
+                // currentGameUpdate.style.color = '#fa4f00';
+            }
+
+            if (gameCount === 5) {
+                if (computerWinningCount > playerWinningCount) {
+                    finalWinner.textContent = 'Sorry you Lose';
+                    finalWinner.style.color = '#fa4f00';
+                    gameOverBox.style.display = 'flex';
+                } else {
+                    finalWinner.textContent = 'Congrats you Won';
+                    finalWinner.style.color = '#00fa85';
+                    gameOverBox.style.display = 'flex';
+                }
+            }
         }
-    }
+    });
+});
 
-    console.log(`Computer Won: ${compWinningCount}`);
-    console.log(`Player Won: : ${playerWinningCount}`);
-};
+const playAgain = document.getElementById('play-again');
+playAgain.addEventListener('click', () => {
+    location.reload();
+});
